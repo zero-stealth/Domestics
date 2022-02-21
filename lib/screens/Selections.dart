880af7c5bc70c0f718df697216c650d304fd9159
Inffinite <louislaizer4@gmail.com>
2022-02-21 12:30:45 +0300

    :hammer: Built dialog component

--- lib/screens/Selections.dart
+++ lib/screens/Selections.dart
@@ -72,6 +72,36 @@ class _SelectionsState extends State<Selections> {
     }
   }
 
+  void _popup() {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return Dialog(
+            title: 'Tags',
+            content: 'You must select atleast 4 tags to continue',
+            continueWidget: Container(
+                      width: MediaQuery.of(context).size.width - 80,
+                      child: CupertinoButton(
+                        padding: const EdgeInsets.all(10.0),
+                        color: const Color(0xffda4b2e).withOpacity(0.1),
+                        child: const Text(
+                          'Got it',
+                          style: TextStyle(
+                            color: Color(0xffda4b2e),
+                            fontSize: 16.0,
+                            fontFamily: 'SFT-Regular',
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -218,89 +248,80 @@ class _SelectionsState extends State<Selections> {
                 for (var m in selected) {
                   log(m);
                 }
+
+                if (selected.length < 4) {
+                  _popup();
+                }
               },
             ),
           ),
         ),
-        //Dialog(),
+        // Dialog(status: true),
       ])),
     );
   }
 }
 
 class Dialog extends StatelessWidget {
-  const Dialog({
-    Key? key,
-  }) : super(key: key);
+  final String title;
+  final String content;
+  // final Widget cancelWidget;
+  final Widget continueWidget;
+
+  Dialog({ 
+    required this.title, 
+    required this.content,
+    // required this.cancelWidget,
+    required this.continueWidget,
+    });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: double.infinity,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: const Color(0xffda4b2e).withOpacity(0.6),
-      ),
-      child: Center(
-        child: Wrap(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width - 40,
-              padding: EdgeInsets.all(20.0),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Colors.white),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'Tags',
-                    textAlign: TextAlign.start,
-                    style: TextStyle(
-                      fontFamily: 'SFD-Bold',
-                      color: Color(0xff2b2c2e),
-                      fontSize: 18.0,
-                    ),
+    return Center(
+      child: Wrap(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width - 40,
+            padding: EdgeInsets.all(20.0),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: Colors.white),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontFamily: 'SFD-Bold',
+                    decoration: TextDecoration.none,
+                    fontWeight: FontWeight.normal,
+                    color: Color(0xff363831),
+                    fontSize: 18.0,
                   ),
-                  const SizedBox(height: 10.0),
-                  const Text(
-                    'You must select atleast 4 tags to continue.',
-                    textAlign: TextAlign.start,
-                    style: TextStyle(
-                      fontFamily: 'SFT-Regular',
-                      color: Color(0xff2b2c2e),
-                      fontSize: 16.0,
-                    ),
+                ),
+                const SizedBox(height: 10.0),
+                Text(
+                  content,
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontFamily: 'SFT-Regular',
+                    fontWeight: FontWeight.normal,
+                    decoration: TextDecoration.none,
+                    color: Color(0xff736c7a),
+                    fontSize: 16.0,
                   ),
-                  const SizedBox(height: 20.0),
-                  Row(
-                    children: [
-                      Container(
-                        width: MediaQuery.of(context).size.width - 80,
-                        child: CupertinoButton(
-                          padding: const EdgeInsets.all(10.0),
-                          color: const Color(0xffda4b2e).withOpacity(0.1),
-                          child: const Text(
-                            'Got it',
-                            style: TextStyle(
-                              color: Color(0xffda4b2e),
-                              fontSize: 16.0,
-                              fontFamily: 'SFT-Regular',
-                            ),
-                          ),
-                          onPressed: () {
-                            
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
+                ),
+                const SizedBox(height: 20.0),
+                Row(
+                  children: [
+                    continueWidget
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
