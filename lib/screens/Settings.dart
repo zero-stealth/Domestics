40e6489960b4c58f661c4aa6a5dbbca36cd55d17
Inffinite <louislaizer4@gmail.com>
2022-02-23 20:33:30 +0300

    :hammer: Created a feedback modal

--- lib/screens/Settings.dart
+++ lib/screens/Settings.dart
@@ -7,6 +7,91 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  _feedbackModal(context) {
+    showModalBottomSheet(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(15.0),
+        ),
+      ),
+      isScrollControlled: true,
+      context: context,
+      builder: (context) => Padding(
+        padding: MediaQuery.of(context).viewInsets,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 50.0,
+                    height: 4.0,
+                    margin: EdgeInsets.only(top: 10.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50.0),
+                      color: const Color(0xff8e8e90).withOpacity(0.3),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 15.0),
+              const Text(
+                'Feedback',
+                style: TextStyle(
+                  fontFamily: 'SFD-Bold',
+                  color: Color(0xff262626),
+                  fontSize: 22.0,
+                ),
+              ),
+              const SizedBox(height: 8.0),
+              const Text(
+                'Send us your thoughts, ideas or say hi. Life is too short.',
+                style: TextStyle(
+                  fontFamily: 'SFT-Regular',
+                  color: Color(0xff8e8e90),
+                  fontSize: 14.0,
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              CupertinoTextField(
+                padding: const EdgeInsets.all(20.0),
+                placeholder: 'Type something',
+                minLines: 4,
+                maxLines: 4,
+                decoration: BoxDecoration(
+                    color: const Color(0xff8e8e90).withOpacity(0.1),
+                    borderRadius: BorderRadius.circular(10.0)),
+              ),
+              const SizedBox(height: 20.0),
+              Container(
+                width: double.infinity,
+                child: CupertinoButton(
+                  color: Colors.blueAccent,
+                  child: const Text(
+                    'Send',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                      fontFamily: 'SFT-Regular',
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+              const SizedBox(height: 30.0),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,7 +142,7 @@ class _SettingsState extends State<Settings> {
                   top: 10.0,
                 ),
                 child: GestureDetector(
-                  onTap: (){
+                  onTap: () {
                     Navigator.pop(context);
                   },
                   child: Icon(
@@ -123,10 +208,15 @@ class _SettingsState extends State<Settings> {
                                 color: Color(0xff262626),
                                 name: 'Stats'),
                             MyDivider(),
-                            SettingsItem(
-                                icon: CupertinoIcons.chat_bubble,
-                                color: Color(0xff262626),
-                                name: 'Feedback'),
+                            InkWell(
+                              onTap: () {
+                                _feedbackModal(context);
+                              },
+                              child: SettingsItem(
+                                  icon: CupertinoIcons.chat_bubble,
+                                  color: Color(0xff262626),
+                                  name: 'Feedback'),
+                            ),
                             MyDivider(),
                             SettingsItem(
                                 icon: CupertinoIcons.bag_badge_minus,
